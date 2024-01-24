03ee976a349f66c6425d448e1fb1f8d7ca769ce1
Martin H <mlmh3113@hotmail.com>
2024-01-24 02:30:24 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'https://mlmh3113.github.io/personal-trainer',
+  base: '/personal-trainer',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
